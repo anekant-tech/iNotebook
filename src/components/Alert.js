32655b7e87c8ddb6a8caad6719972e40e735716c
anekant jain <anekantjain2016@gmail.com>
2021-12-31 15:54:23 +0530

    completed

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function Alert(props) {
-    return (
-        <div>
-            <div>
-            <div class="alert alert-primary" role="alert">
-                {props.message}
-            </div>
+  const capitalize = (word) => {
+    if (word === "danger") {
+      word = "error";
+    }
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  };
+  return (
+    <div style={{ height: "50px" }}>
+      {props.alert && (
+        <div
+          className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+          role="alert"
+        >
+          <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
         </div>
-        </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default Alert
+export default Alert;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,13 @@
-import React from 'react'
-import Notes from './Notes';
+import React from "react";
+import Notes from "./Notes";
 
-function Home() {
-    return (
-        <div>
-           
-
-            <Notes/>
-            
-        </div>
-    )
+function Home(props) {
+  const { showAlert } = props;
+  return (
+    <div>
+      <Notes showAlert={showAlert} />
+    </div>
+  );
 }
 
-export default Home
-
+export default Home;
